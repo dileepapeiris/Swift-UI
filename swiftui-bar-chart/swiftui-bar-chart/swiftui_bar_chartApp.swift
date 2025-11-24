f192deec2d5bce7f3bbbef5592aef7ae7e4e9dae
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-11-24 20:54:05 +0530

    Add main app entry point for SwiftUI bar chart
    Introduces swiftui_bar_chartApp as the main application entry point, setting ContentView as the initial view in the SwiftUI app.

---
+++ swiftui-bar-chart/swiftui-bar-chart/swiftui_bar_chartApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_bar_chartApp.swift
+//  swiftui-bar-chart
+//
+//  Created by Dileepa Peiris on 2025-11-24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_bar_chartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
