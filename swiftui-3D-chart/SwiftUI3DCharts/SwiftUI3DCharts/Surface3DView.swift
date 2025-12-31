cf11f25ca9d894a1d2ed56e898778f6e2276fc4d
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 22:58:24 +0530

    Add header to Surface3DView
    Introduced a VStack with a '3D Surface Plot' title in Surface3DView to provide a descriptive header for the 3D chart view.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Surface3DView.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Surface3DView.swift
@@ -2,4 +2,9 @@ import SwiftUI
 import SceneKit
 
 struct Surface3DView: View {
+    var body: some View {
+        VStack {
+            Text("3D Surface Plot")
+                .font(.title2).bold()
+                .padding(.top)
     }
