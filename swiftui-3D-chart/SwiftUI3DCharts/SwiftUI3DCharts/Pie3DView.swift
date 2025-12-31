71869dc378c0d085e1b0b7f93660c34bac38d4b8
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 22:55:19 +0530

    Add title to 3D Pie Chart view
    Introduced a '3D Pie Chart' title at the top of the Pie3DView using a styled Text view within a VStack.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Pie3DView.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Pie3DView.swift
@@ -4,4 +4,9 @@ struct Pie3DView: View {
     var data: [ChartData]
     @State private var rotation: Double = 45
     
+    var body: some View {
+        VStack {
+            Text("3D Pie Chart")
+                .font(.title2).bold()
+                .padding(.top)
 }
