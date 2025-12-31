ad3c226207b5f1283af82a4426f059f7f1378e01
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 22:51:01 +0530

    Start implementing Bar3DView body layout
    Added a VStack with a title for the 3D Bar Chart in the Bar3DView's body. This sets up the initial structure for the view.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Bar3DView.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/Bar3DView.swift
@@ -5,3 +5,9 @@ struct Bar3DView: View {
     @State private var rotation: Double = -15
     @State private var tilt: Double = 15
     
+    var body: some View {
+        VStack {
+            Text("3D Bar Chart")
+                .font(.title2).bold()
+                .padding(.top)
+            
