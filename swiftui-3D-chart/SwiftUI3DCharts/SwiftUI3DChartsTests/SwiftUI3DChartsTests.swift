dfc2f4a8966598ec342a18399cb9fc3161e9ebc8
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 23:01:17 +0530

    Add initial test structure for SwiftUI3DCharts
    Introduced a basic test struct using the Testing framework and imported the main module for testing. This sets up the foundation for writing unit tests in SwiftUI3DCharts.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DChartsTests/SwiftUI3DChartsTests.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DChartsTests/SwiftUI3DChartsTests.swift
@@ -4,3 +4,14 @@
 //
 //  Created by Dileepa Peiris on 2025-12-24.
 //
+
+import Testing
+@testable import SwiftUI3DCharts
+
+struct SwiftUI3DChartsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
