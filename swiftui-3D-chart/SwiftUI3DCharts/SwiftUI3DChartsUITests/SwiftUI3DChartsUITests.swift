f944ffb53c85512941f004a707ace62d10cde23b
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 23:02:44 +0530

    Add setUpWithError to UI test class
    Introduced the setUpWithError method in SwiftUI3DChartsUITests to allow for setup before each test method. Currently, the method contains a placeholder comment.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DChartsUITests/SwiftUI3DChartsUITests.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DChartsUITests/SwiftUI3DChartsUITests.swift
@@ -9,4 +9,8 @@ import XCTest
 
 final class SwiftUI3DChartsUITests: XCTestCase {
 
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    }
 }
