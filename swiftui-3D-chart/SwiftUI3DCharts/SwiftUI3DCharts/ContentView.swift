375d0ba2836c75c4af00dde6b8a6e6cda4e29041
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-31 22:53:25 +0530

    Add 3D Bar Chart navigation link to ContentView
    Introduced a List section in ContentView with a navigation link to the 3D Bar Chart view using mock data. This enhances navigation and discoverability of the 3D chart feature.

--- swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/ContentView.swift
+++ swiftui-3D-chart/SwiftUI3DCharts/SwiftUI3DCharts/ContentView.swift
@@ -3,6 +3,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
+            List {
+                Section(header: Text("SwiftUI 3D Transforms")) {
+                    NavigationLink(destination: Bar3DView(data: ChartData.mockBarData)) {
+                        Label("3D Bar Chart", systemImage: "chart.bar.fill")
+                    }
+                    
     }
 }
 }
